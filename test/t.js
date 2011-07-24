f073b97b9f1e2d5415e2ae1076ef9a9fab96ceb2
James Halliday <mail@substack.net>
2011-07-24 03:36:38 -0700

    test passing with expresso

--- test/t.js
+++ test/t.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var tAssert = require('./t/assert');
 var intestine = require('../')
+var Hash = require('hashish');
 
 exports.suite = function () {
     var suite = intestine();
@@ -24,13 +25,54 @@ exports.suite = function () {
     var asserts = {};
     
     suite.on('assert', function (res, test) {
-console.dir(test);
         if (!asserts[test.name]) asserts[test.name] = []
         asserts[test.name].push(res);
     });
     
+    var iv = setTimeout(function () {
+        assert.fail('never finished');
+    }, 15000);
+    
     suite.on('end', function () {
-        console.dir(asserts);
+        clearTimeout(iv);
+        
+        assert.deepEqual(
+            asserts.first.map(function (x) {
+                return Hash.extract(x, [
+                    'ok', 'found', 'wanted', 'type', 'name'
+                ])
+            }),
+            [
+                {
+                    name : undefined,
+                    type : 'ok',
+                    ok : true,
+                    found : true,
+                    wanted : true
+                },
+                {
+                    name : '1 plus 2',
+                    type : 'equal',
+                    ok : true,
+                    found : 3,
+                    wanted : 3
+                }
+            ]
+        );
+        
+        assert.equal(asserts.second.length, 1);
+        assert.deepEqual(
+            Hash.extract(asserts.second[0], [
+                'ok', 'found', 'wanted', 'type', 'name'
+            ]),
+            {
+                name : undefined,
+                type : 'equal',
+                ok : false,
+                found : 4,
+                wanted : 3
+            }
+        );
     });
     
     suite.on('error', function (err, test) {
@@ -41,7 +83,7 @@ console.dir(test);
         var test = require('testling');
         test('first', function (t) {
             t.ok(true);
-            t.equal(1 + 2, 3);
+            t.equal(1 + 2, 3, '1 plus 2');
             t.end();
         });
     }.toString() + ')()', { filename : 'first.js' });
